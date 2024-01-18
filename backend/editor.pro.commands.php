b3562fc873559fabc8e8eb6886c25a58eff0fe4f
Dirk Schwartzer <info@inter-data.de>
2024-01-18 10:45:20 +0100

    Added session management and elFinder

---
+++ backend/editor.pro.commands.php
@@ -0,0 +1,19 @@
+<?php
+namespace editor;
+
+define("GIT_COMMAND", "cd ".ROOT_DIR." && git add --all && git commit -m ");
+define("GIT_PUSH_COMMAND", " && git push".STDERR_REDIRECT);
+define("GIT_RESET_COMMAND", "cd ".ROOT_DIR." && git reset --hard".STDERR_REDIRECT);
+
+function versioning($data)
+{
+    $msg = "generated commit by HugoCMS Editor";
+    if(isset($data['commsg'])) $msg = $data['commsg'];
+    execute(GIT_COMMAND.'"'.$msg.'"'.GIT_PUSH_COMMAND);
+    writeLog(GIT_COMMAND.'"'.$msg.'"'.GIT_PUSH_COMMAND);
+}
+
+function restore()
+{
+    execute(GIT_RESET_COMMAND);
+}
